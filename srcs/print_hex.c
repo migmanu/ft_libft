641334594df165d77de5902ce7600ddadeffc193
migmanu42 <jmanuelmigoya@gmail.com>
2023-06-19 19:03:27 +0200

    work

--- srcs/print_hex.c
+++ srcs/print_hex.c
@@ -6,11 +6,34 @@
 /*   By: jmigoya- <jmigoya-@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 17:56:01 by jmigoya-          #+#    #+#             */
-/*   Updated: 2023/06/19 17:56:12 by jmigoya-         ###   ########.fr       */
+/*   Updated: 2023/06/19 19:02:07 by jmigoya-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	print_hex()
-{
+#include "../include/ft_printf.h"
 
+int	print_hex_low(long n)
+{
+	int	l;
+
+	l = 0;
+	if (n < 0)
+	{
+
+	}
+	if (n > 9)
+	{
+
+	}
+	else
+	{
+
+	}
+	return (l);
+}
+
+int	main(void)
+{
+	long int	a = 2147483648;
+	printf("%x", a);
 }
