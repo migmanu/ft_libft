19c6ccd758030269d0302eed0e139e5c7093e0f6
migmanu <jmanuelmigoya@gmail.com>
2024-05-26 20:52:12 +0200

    fix: styling

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: jmigoya- <jmigoya-@student.42berlin.d      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/05/06 17:23:11 by jmigoya-          #+#    #+#              #
-#    Updated: 2024/05/26 20:32:21 by jmigoya-         ###   ########.fr        #
+#    Updated: 2024/05/26 20:39:29 by jmigoya-         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -66,35 +66,3 @@ fclean: clean
 re: fclean all
 
 .PHONY : clean fclean re
-# NAME = libftprintf.a
-# CC = gcc
-# CFLAGS = -Werror -Wall -Wextra 
-# HEADER = ./include/ft_printf.h
-# LIBFT = ./include/libft
-#
-# FILES = ft_printf.c printfutils.c print_hex.c print_ptr.c
-#
-# FILES_PATH = ./srcs/
-#
-# SRCS = $(addprefix $(FILES_PATH), $(FILES))
-#
-# OBJS = $(SRCS:.c=.o)
-#
-# all: $(NAME)
-#
-# $(NAME): $(OBJS)
-# 	make -C $(LIBFT)
-# 	cp include/libft/libft.a .
-# 	mv libft.a $(NAME)
-# 	ar rcs $(NAME) $(OBJS)
-#
-# clean :
-# 	rm -f $(OBJS)
-# 	make clean -C $(LIBFT)
-#
-# fclean : clean
-# 	rm -f $(NAME)
-# 	make fclean -C $(LIBFT)
-#
-# re : fclean all
-#
--- src/main.c
+++ src/main.c
@@ -1,28 +1,44 @@
-# include "../include/ft_printf.h"
-# include <limits.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmigoya- <jmigoya-@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/26 20:40:30 by jmigoya-          #+#    #+#             */
+/*   Updated: 2024/05/26 20:43:08 by jmigoya-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-static void ft_printheader(char *arg)
+#include "../include/ft_printf.h"
+#include <limits.h>
+
+static void	ft_printheader(char *arg)
 {
-    printf("\n\n---------------------------------------\n");
-    printf("\t\t %s\n", arg);
-    printf("---------------------------------------\n\n");
+	printf("\n\n---------------------------------------\n");
+	printf("\t\t %s\n", arg);
+	printf("---------------------------------------\n\n");
 }
 
 void	test_c(void)
 {
-	int	x, y;
+	int	x;
+	int	y;
+
 	x = 0;
 	y = 0;
 	ft_printheader("Test %c");
 	printf("Testing: \t(\"%%c\", 'a')\n");
-	x +=    printf("  Ft\t:\t%c", 'x');
+	x += printf("  Ft\t:\t%c", 'x');
 	y += ft_printf("  Ft\t:\t%c", 'x');
 	printf("Printf = %d, ft_printf = %d\n\n", x, y);
 	x = 0;
 	y = 0;
 	printf("Testing: (\"%%c\", '0')\n");
-	x +=    printf("%c", '0'); printf("\n");
-	y += ft_printf("%c", '0'); printf("\n");
+	x += printf("%c", '0');
+	printf("\n");
+	y += ft_printf("%c", '0');
+	printf("\n");
 	printf("Printf = %d, ft_printf = %d\n\n", x, y);
 	x = 0;
 	y = 0;
@@ -32,171 +48,205 @@ void	test_c(void)
 	printf("Printf = %d, ft_printf = %d\n\n", x, y);
 }
 
-void    test_s(void)
+void	test_s(void)
 {
-    int	x, y;
+	int	x;
+	int	y;
+
 	x = 0;
 	y = 0;
-    ft_printheader("Test %s");
-    printf("Testing: \t(\"%%s\n\", 'TEST')\n");
-	x +=    printf("  Ft\t:\t%s\n", "TEST");
+	ft_printheader("Test %s");
+	printf("Testing: \t(\"%%s\n\", 'TEST')\n");
+	x += printf("  Ft\t:\t%s\n", "TEST");
 	y += ft_printf("  Ft\t:\t%s\n", "TEST");
 	printf("Printf = %d, ft_printf = %d\n\n", x, y);
 	x = 0;
 	y = 0;
 	printf("Testing: (\" NULL %%s NULL %%s\", Test , Test)\n");
-	x +=    printf(" NULL %s NULL %s", "Test", "Test"); printf("\n");
-	y += ft_printf(" NULL %s NULL %s", "Test", "Test"); printf("\n");
+	x += printf(" NULL %s NULL %s", "Test", "Test");
+	printf("\n");
+	y += ft_printf(" NULL %s NULL %s", "Test", "Test");
+	printf("\n");
 	printf("Printf = %d, ft_printf = %d\n\n", x, y);
 }
 
 void	test_p(void)
 {
-	int	x, y;
-    void *ptr = "Test";
+	void	*ptr;
+	int		x;
+	int		y;
+
+	ptr = "Test";
 	x = 0;
 	y = 0;
 	ft_printheader("Test %p");
 	printf("Testing: \t(\"%%p\", ptr)\n");
-	x +=    printf("  Ft\t:\t%p\n", ptr);
+	x += printf("  Ft\t:\t%p\n", ptr);
 	y += ft_printf("  Ft\t:\t%p\n", ptr);
 	printf("Printf = %d, ft_printf = %d\n\n", x, y);
 }
 
-void    test_i(void)
+void	test_i(void)
 {
-    int	x, y;
+	int	x;
+	int	y;
+
 	x = 0;
 	y = 0;
-    ft_printheader("Test %i");
-    printf("Testing: \t(\"%%i\", 9)\n");
-	x +=    printf(" %i ", 9); printf("\n");
-	y += ft_printf(" %i ", 9); printf("\n");
+	ft_printheader("Test %i");
+	printf("Testing: \t(\"%%i\", 9)\n");
+	x += printf(" %i ", 9);
+	printf("\n");
+	y += ft_printf(" %i ", 9);
+	printf("\n");
 	printf("Printf = %d, ft_printf = %d\n\n", x, y);
 	x = 0;
 	y = 0;
 	printf("Testing: \t(\"%%i\", 10)\n");
-	x +=    printf(" %i ", 10); printf("\n");
-	y += ft_printf(" %i ", 10); printf("\n");
+	x += printf(" %i ", 10);
+	printf("\n");
+	y += ft_printf(" %i ", 10);
+	printf("\n");
 	printf("Printf = %d, ft_printf = %d\n\n", x, y);
-    x = 0;
+	x = 0;
 	y = 0;
 	printf("Testing: \t(\"%%i\", 100)\n");
-	x +=    printf(" %i ", 100); printf("\n");
-	y += ft_printf(" %i ", 100); printf("\n");
+	x += printf(" %i ", 100);
+	printf("\n");
+	y += ft_printf(" %i ", 100);
+	printf("\n");
 	printf("Printf = %d, ft_printf = %d\n\n", x, y);
-    x = 0;
+	x = 0;
 	y = 0;
 	printf("Testing: \t(\"%%i\", 1001)\n");
-	x +=    printf(" %i ", 1001); printf("\n");
-	y += ft_printf(" %i ", 1001); printf("\n");
+	x += printf(" %i ", 1001);
+	printf("\n");
+	y += ft_printf(" %i ", 1001);
+	printf("\n");
 	printf("Printf = %d, ft_printf = %d\n\n", x, y);
 }
 
-void    test_u(void)
+void	test_u(void)
 {
-    int	x, y;
+	int	x;
+	int	y;
+
 	x = 0;
 	y = 0;
-    ft_printheader("Test %u");
-    printf("Testing: \t(\"%%u\", 9)\n");
-	x +=    printf(" %u ", 9); printf("\n");
-	y += ft_printf(" %u ", 9); printf("\n");
+	ft_printheader("Test %u");
+	printf("Testing: \t(\"%%u\", 9)\n");
+	x += printf(" %u ", 9);
+	printf("\n");
+	y += ft_printf(" %u ", 9);
+	printf("\n");
 	printf("Printf = %d, ft_printf = %d\n\n", x, y);
 	x = 0;
 	y = 0;
 	printf("Testing: \t(\"%%u\", -1)\n");
-	x +=    printf(" %u ", -1); printf("\n");
-	y += ft_printf(" %u ", -1); printf("\n");
+	x += printf(" %u ", -1);
+	printf("\n");
+	y += ft_printf(" %u ", -1);
+	printf("\n");
 	printf("Printf = %d, ft_printf = %d\n\n", x, y);
-    x = 0;
+	x = 0;
 	y = 0;
 	printf("Testing: \t(\"%%u\", 100)\n");
-	x +=    printf(" %u ", 100); printf("\n");
-	y += ft_printf(" %u ", 100); printf("\n");
+	x += printf(" %u ", 100);
+	printf("\n");
+	y += ft_printf(" %u ", 100);
+	printf("\n");
 	printf("Printf = %d, ft_printf = %d\n\n", x, y);
-    x = 0;
+	x = 0;
 	y = 0;
 	printf("Testing: \t(\"%%u\", 1001)\n");
-	x +=    printf(" %u ", 1001); printf("\n");
-	y += ft_printf(" %u ", 1001); printf("\n");
+	x += printf(" %u ", 1001);
+	printf("\n");
+	y += ft_printf(" %u ", 1001);
+	printf("\n");
 	printf("Printf = %d, ft_printf = %d\n\n", x, y);
 }
 
-void    test_percent(void)
+void	test_percent(void)
 {
-    int	x, y;
+	int	x;
+	int	y;
+
 	x = 0;
 	y = 0;
-    ft_printheader("Test %%");
-    printf("Testing: \t(\"%%%%\")\n");
-	x +=    printf(" %%%% "); printf("\n");
-	y += ft_printf(" %%%% "); printf("\n");
+	ft_printheader("Test %%");
+	printf("Testing: \t(\"%%%%\")\n");
+	x += printf(" %%%% ");
+	printf("\n");
+	y += ft_printf(" %%%% ");
+	printf("\n");
 	printf("Printf = %d, ft_printf = %d\n\n", x, y);
 	x = 0;
 	y = 0;
-    printf("Testing: \t(\"%%%%%%%%\")\n");
-	x +=    printf(" %%%%%%%% "); printf("\n");
-	y += ft_printf(" %%%%%%%% "); printf("\n");
+	printf("Testing: \t(\"%%%%%%%%\")\n");
+	x += printf(" %%%%%%%% ");
+	printf("\n");
+	y += ft_printf(" %%%%%%%% ");
+	printf("\n");
 	printf("Printf = %d, ft_printf = %d\n\n", x, y);
 	x = 0;
 	y = 0;
-    printf("Testing: \t(\"%@\")\n");
-	x +=    printf("Test %@"); printf("\n");
-	y += ft_printf("Test %@"); printf("\n");
+	printf("Testing: \t(\"%@\")\n");
+	x += printf("Test %@");
+	printf("\n");
+	y += ft_printf("Test %@");
+	printf("\n");
 	printf("Printf = %d, ft_printf = %d\n\n", x, y);
 }
 
-
-void    test_NULL(void)
+void	test_NULL(void)
 {
-    int	x, y;
+	int	x;
+	int	y;
+
 	x = 0;
 	y = 0;
-    ft_printheader("Test NULL");
-    printf("Testing: \t(NULL)\n");
-	x +=    printf(NULL); printf("\n");
-	y += ft_printf(NULL); printf("\n");
+	ft_printheader("Test NULL");
+	printf("Testing: \t(NULL)\n");
+	x += printf(NULL);
+	printf("\n");
+	y += ft_printf(NULL);
+	printf("\n");
 	printf("Printf = %d, ft_printf = %d\n\n", x, y);
 	x = 0;
 	y = 0;
-    printf("Testing: \t(0)\n");
-	x +=    printf(0); printf("\n");
-	y += ft_printf(0); printf("\n");
-	printf("Printf = %d, ft_printf = %d\n\n", x, y);
-}
-
-void    test_MIX(void)
-{
-    int	x, y;
-	x = 0;
-	y = 0;
-    ft_printheader("Test MIX");
-    printf("Testing: (\"%% %%i %% %%s %%c\",12, \"Test\", 'r')\n");
-	x +=    printf("%i %% %s %c %",12, "Test", 'r'); printf("\n");
-	y += ft_printf("%i %% %s %c %",12, "Test", 'r'); printf("\n");
+	printf("Testing: \t(0)\n");
+	x += printf(0);
+	printf("\n");
+	y += ft_printf(0);
+	printf("\n");
 	printf("Printf = %d, ft_printf = %d\n\n", x, y);
 }
 
-
-int main(void)
+void	test_MIX(void)
 {
-    //test_c();
+	int	x;
+	int	y;
 
-    //test_s();
+	x = 0;
+	y = 0;
+	ft_printheader("Test MIX");
+	printf("Testing: (\"%% %%i %% %%s %%c\",12, \"Test\", 'r')\n");
+	x += printf("%i %% %s %c %", 12, "Test", 'r');
+	printf("\n");
+	y += ft_printf("%i %% %s %c %", 12, "Test", 'r');
+	printf("\n");
+	printf("Printf = %d, ft_printf = %d\n\n", x, y);
+}
 
-    //test_p();
-
-    //test_i();
-
-    //test_u();
-
+int	main(void)
+{
+	// test_c();
+	// test_s();
+	// test_p();
+	// test_i();
+	// test_u();
 	test_percent();
-
 	test_NULL();
-
 	test_MIX();
-
-
-return (0);
+	return (0);
 }
